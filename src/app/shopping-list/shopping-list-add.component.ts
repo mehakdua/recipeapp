ad318aa9e26cd3630b94c767db7a6c4c3e2ed433
mehakdua <mdua798@gmail.com>
2017-03-27 14:37:51 +0530

    array for shooping list

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Ingredient} from '../ingredient';
 @Component({
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
 export class ShoppingListAddComponent implements OnInit {
-
+  items:Ingredient[] = [];
   constructor() { }
 
   ngOnInit() {
